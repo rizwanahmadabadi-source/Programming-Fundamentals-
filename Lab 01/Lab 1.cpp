df21edb7936f252b867d07c13d08d04727ac8ba6
rizwanahmadabadi-source <rizwanahmadabadi@gmail.com>
2025-11-27 00:17:19 +0500

    Add files via upload

---
+++ Lab 01/Lab 1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	// Question 2
+	
+	
+	cout<<"*********************************************************"<<endl;
+	cout<<"*\t"<<"Name:\t\tRizwan Ali\t\t\t*\n";
+	cout<<"*\t"<<"Roll No:\t40\t\t\t\t*\n";
+	cout<<"*\t"<<"Department:\tSoftware Engineering Technalogy\t*\n";
+	cout<<"*\t"<<"Adress:\t\tSkardu Baltistan\t\t*\n";
+	cout<<"*\t"<<"Hobby:\t\tVideo Editing\t\t\t*\n";
+	cout<<"*\t"<<"Qualification:\tFAc\t\t\t\t*\n";
+	cout<<"*\t"<<"SSC Mark:\t726\t\t\t\t*\n";
+	cout<<"*\t"<<"HSSC Marks:\t600\t\t\t\t*\n";
+	cout<<"*********************************************************"<<endl;
+	
+         // Question 2 
+         
+         
+    int A,B;
+    int sum,sub,mult,div;
+    cout<<"enter first number"<<endl;
+    cin>>A;
+	cout<<"enter the second number"<<endl;
+	cin>>B;
+	sum=A+B;
+	cout<<"the sum of A and B is ="<<sum<<endl;
+	sub=A-B;
+	cout<<"the sub of A and B is ="<<sub<<endl;
+	mult=A*B;
+	cout<<"the mult of A amd B is ="<<mult<<endl;
+	div=A/B;
+	cout<<"the div of A and B is ="<<div<<endl;
+	return 0;
+
+}	
+	
